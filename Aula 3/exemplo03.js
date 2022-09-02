4799a6af8d12b9f6e28e86d1685b6b6fc9b2847d
LucasMarafon <111541988+LucasMarafon@users.noreply.github.com>
2022-09-01 21:43:38 -0300

    Add files via upload

---
+++ Aula 3/exemplo03.js
@@ -0,0 +1,13 @@
+const x = 10
+let y = 10
+
+console.log("Adição -> " + (x + 10))
+console.log("Subtração -> " + (x - 10))
+console.log("Multiplicação -> " + (x * 10))
+console.log("Divisão -> " + (x / 10))
+console.log("Expoente -> " + (x**2))
+console.log("Módulo -> " + (x % 10))
+console.log("Incremento anterior -> " + (++y))
+console.log("Incremento posterior -> " + (y++))
+console.log("Incremento anterior -> " + (--y))
+console.log("Incremento posterior -> " + (y--))
